505f493a492c58db425bd23703e36c4573d7efdd
JOSE ALEJANDRO PACHECO BARCENAS <vatosalfa@gmail.com>
2025-07-20 22:27:41 +0000

    Por favor, ve al archivo de configuración de Firebase (usualmente ubicad

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,17 +5,18 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAsUWZLiMR7sVkL0of4-i09-6zWfBHFjSUY",
+  apiKey: "AIzaSyAsUWZLiMR7sVkL0of4i09-6zWfBHFjSUY",
   authDomain: "agenda-1ae08.firebaseapp.com",
   projectId: "agenda-1ae08",
-  storageBucket: "agenda-1ae08.appspot.com",
+  storageBucket: "agenda-1ae08.firebasestorage.app",
   messagingSenderId: "34221543030",
   appId: "1:34221543030:web:b46655e3ef8d7de4539957",
   measurementId: "G-MZC8TZBYFL"
 };
 
-// Initialize Firebase for SSR
+// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 const auth = getAuth(app);

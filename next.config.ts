45df001787d62b8feff35a0da4521f477549fc9f
JOSE ALEJANDRO PACHECO BARCENAS <vatosalfa@gmail.com>
2025-09-16 18:14:07 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -29,6 +29,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'api.twilio.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };

bbcac1abd43b3c016a8f9ceb4f009912f123cc58
JOSE ALEJANDRO PACHECO BARCENAS <vatosalfa@gmail.com>
2025-09-19 05:46:38 +0000

    INTENTE SUBIRLO Y ME APARECIO ESE ERROR

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -3,14 +3,7 @@ import * as admin from 'firebase-admin';
 
 // Check if the app is already initialized to prevent errors
 if (admin.apps.length === 0) {
-  try {
-    admin.initializeApp({
-      credential: admin.credential.applicationDefault(),
-      storageBucket: "agenda-1ae08.appspot.com", // Correct format
-    });
-  } catch (error: any) {
-    console.error("Firebase Admin Initialization Error: ", error.message);
-  }
+    admin.initializeApp();
 }
 
 const adminDb = admin.firestore();

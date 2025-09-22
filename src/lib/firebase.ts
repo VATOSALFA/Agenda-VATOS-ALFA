5865d3eed95ddf9b54bce5fe0c87ecb6ebb3ec11
JOSE ALEJANDRO PACHECO BARCENAS <vatosalfa@gmail.com>
2025-09-22 20:00:27 +0000

    mira abri la consola del navegador por si ayuda en algo a identificar el

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBFVn5uDL7RkAQJdXoGrTitwqcl6kSb-vE",
-  authDomain: "agenda-vatos-alfa-v1.firebaseapp.com",
-  projectId: "agenda-vatos-alfa-v1",
-  storageBucket: "agenda-vatos-alfa-v1.appspot.com",
-  messagingSenderId: "964979094210",
-  appId: "1:964979094210:web:ebff0199f5c4ec3d98015b",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);

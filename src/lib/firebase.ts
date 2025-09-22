613638b01be58b8c514f0e4566c29ea1ab0d1428
JOSE ALEJANDRO PACHECO BARCENAS <vatosalfa@gmail.com>
2025-09-22 20:39:00 +0000

    El erro de inicio de sesión continua, estos son los errores en la consol

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAsUWZLIMR7sVkL0of4i09-6zWfBHFjSUY",
+  authDomain: "agenda-1ae08.firebaseapp.com",
+  projectId: "agenda-1ae08",
+  storageBucket: "agenda-1ae08.appspot.com",
+  messagingSenderId: "56391456543",
+  appId: "1:56391456543:web:5d79c3f5904acb8e61198e",
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);

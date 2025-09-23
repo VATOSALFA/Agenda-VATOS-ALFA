ccd7552a08cb1543de9928253034c9f5a06bd0a0
JOSE ALEJANDRO PACHECO BARCENAS <vatosalfa@gmail.com>
2025-09-23 00:45:36 +0000

    antes de hacer eso ayudame para subir el paquete por favor

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // ¡NADA de `output: "export"` ni `images: { unoptimized: true }`!
-  typescript: { ignoreBuildErrors: true }, // opcional/temporal
-};
-
-export default nextConfig;

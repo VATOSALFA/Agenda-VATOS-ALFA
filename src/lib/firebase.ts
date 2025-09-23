22cf47a3131a57056f52fe30bdaf3895755b2b2e
JOSE ALEJANDRO PACHECO BARCENAS <vatosalfa@gmail.com>
2025-09-23 22:23:06 +0000

    Mi aplicación no puede conectarse a Firestore en el entorno de desarroll

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,6 +4,7 @@ import { initializeApp, getApps, getApp, type FirebaseOptions } from "firebase/a
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 const firebaseConfig: FirebaseOptions = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -16,6 +17,19 @@ const firebaseConfig: FirebaseOptions = {
 
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+// Initialize App Check
+if (typeof window !== 'undefined') {
+  // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+  // is visibly public.
+  (self as any).FIREBASE_APPCHECK_DEBUG_TOKEN = process.env.NEXT_PUBLIC_APPCHECK_DEBUG_TOKEN;
+  
+  initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!),
+    isTokenAutoRefreshEnabled: true
+  });
+}
+
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);

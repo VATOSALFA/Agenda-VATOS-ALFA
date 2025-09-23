083ddbc8c4bec8d5694aa817dbcfe47c0907721b
JOSE ALEJANDRO PACHECO BARCENAS <vatosalfa@gmail.com>
2025-09-23 02:03:31 +0000

    Sigue igual ¿Sera algun vinculo roto entre el proyecto y github?

--- src/components/providers/auth-root.tsx
+++
@@ -1,8 +0,0 @@
-// src/components/providers/auth-root.tsx
-"use client";
-
-import { AuthProvider } from "@/contexts/auth-context";
-
-export default function AuthRoot({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
-}

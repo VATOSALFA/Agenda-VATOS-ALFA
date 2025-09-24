a19336fe48cc00362b1c7357866c81499a2e5d00
JOSE ALEJANDRO PACHECO BARCENAS <vatosalfa@gmail.com>
2025-09-24 04:58:58 +0000

    ESPERA LOS ULTIMOS 2 CAMBIOS QUE HICISTE aun no los tenias que hacer jaj

--- src/components/sales/mercado-pago-provider.tsx
+++ src/components/sales/mercado-pago-provider.tsx
@@ -4,17 +4,10 @@
 import { initMercadoPago } from '@mercadopago/sdk-react';
 import { ReactNode } from 'react';
 
-const MERCADO_PAGO_PUBLIC_KEY = process.env.NEXT_PUBLIC_MERCADO_PAGO_PUBLIC_KEY;
-
-// Initialize Mercado Pago
-if (MERCADO_PAGO_PUBLIC_KEY) {
-  initMercadoPago(MERCADO_PAGO_PUBLIC_KEY, {
-    locale: 'es-MX'
-  });
-} else {
-    console.error("Mercado Pago Public Key is not configured in environment variables.");
-}
-
+// This is a test public key. Replace with your actual public key in production.
+initMercadoPago('TEST-c4a72008-6134-4536-9c04-7b7076465432', {
+  locale: 'es-MX'
+});
 
 export const MercadoPagoProvider = ({ children }: { children: ReactNode }) => {
     return <>{children}</>;

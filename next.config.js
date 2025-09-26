9dd6223ca6b01c55523051172a7f9967578237d4
JOSE ALEJANDRO PACHECO BARCENAS <vatosalfa@gmail.com>
2025-09-26 20:24:55 +0000

    una vez mas volvio a fallar, encontre esta información y te la paso para

--- next.config.js
+++ next.config.js
@@ -8,34 +8,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'media.twiliocdn.com',
-        port: '',
-        pathname: '/**',
-      },
-       {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;

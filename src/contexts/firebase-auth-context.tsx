963cc36ad796891e78868ff46716a09cc2f3ee9c
JOSE ALEJANDRO PACHECO BARCENAS <vatosalfa@gmail.com>
2025-10-04 17:31:57 +0000

    Ahora mira, inicio sesión con los datos correctos y me manda el mensaje

--- src/contexts/firebase-auth-context.tsx
+++ src/contexts/firebase-auth-context.tsx
@@ -131,7 +131,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     db // Pass db instance through context
   };
   
-  if (loading && !isAuthPage) {
+  if (loading && !isAuthPage && !isPublicPage) {
      return (
       <div className="flex justify-center items-center h-screen bg-muted/40">
         <Loader2 className="h-12 w-12 animate-spin text-primary" />

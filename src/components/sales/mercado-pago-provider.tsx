08890bd1f3629d65e3c42a023d98d5946f91a4c0
JOSE ALEJANDRO PACHECO BARCENAS <vatosalfa@gmail.com>
2025-10-08 00:53:58 +0000

    enterado, ahora creo que el siguiente paso sera configurar mercado pago

--- src/components/sales/mercado-pago-provider.tsx
+++ src/components/sales/mercado-pago-provider.tsx
@@ -4,10 +4,16 @@
 import { initMercadoPago } from '@mercadopago/sdk-react';
 import { ReactNode } from 'react';
 
-// This is a test public key. Replace with your actual public key in production.
-initMercadoPago('TEST-c4a72008-6134-4536-9c04-7b7076465432', {
-  locale: 'es-MX'
-});
+const publicKey = process.env.NEXT_PUBLIC_MERCADO_PAGO_PUBLIC_KEY;
+
+if (publicKey) {
+  initMercadoPago(publicKey, {
+    locale: 'es-MX'
+  });
+} else {
+  console.warn("La clave pública de Mercado Pago no está configurada. Los pagos con tarjeta no funcionarán.");
+}
+
 
 export const MercadoPagoProvider = ({ children }: { children: ReactNode }) => {
     return <>{children}</>;

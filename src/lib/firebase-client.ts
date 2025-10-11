36f7047c63776b46a60170fc312bc931465383a4
JOSE ALEJANDRO PACHECO BARCENAS <vatosalfa@gmail.com>
2025-10-11 23:01:56 +0000

    Revisa si tiene que ver algo con el inicio de sesión, con app check o al

--- src/lib/firebase-client.ts
+++ src/lib/firebase-client.ts
@@ -4,6 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getFunctions, httpsCallable } from "firebase/functions";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 const firebaseConfig: FirebaseOptions = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -16,6 +17,15 @@ const firebaseConfig: FirebaseOptions = {
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+
+// Initialize App Check
+if (typeof window !== 'undefined') {
+  const appCheck = initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!),
+    isTokenAutoRefreshEnabled: true
+  });
+}
+
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);

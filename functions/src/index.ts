f7296753fa4db3a6b0bb0301a9d94a2cdadb16fa
JOSE ALEJANDRO PACHECO BARCENAS <vatosalfa@gmail.com>
2025-10-15 04:22:05 +0000

    Ya solo queda resolver un problema
    [{
    	"resource": "/home/user/studio/s

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 
-import * as functions from 'firebase-functions/v1';
+import * as functions from 'firebase-functions';
 import twilio from 'twilio';
 import * as admin from 'firebase-admin';
 import { format, parseISO } from 'date-fns';

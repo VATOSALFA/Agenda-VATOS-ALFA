07703efa39e9301dacaf3d8f23e2957cc601a78e
JOSE ALEJANDRO PACHECO BARCENAS <vatosalfa@gmail.com>
2025-10-16 01:59:40 +0000

    Try fixing this error: `Build Error: Failed to compile. Error source: sr

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,4 @@
+// This file is intentionally left empty to resolve a routing conflict.
+export default function AppSubLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}

ca093b25b2724839a237c7e9a21257055f246b93
JOSE ALEJANDRO PACHECO BARCENAS <vatosalfa@gmail.com>
2025-10-27 19:05:15 +0000

    Try fixing this error: `Unhandled Runtime Error: ChunkLoadError: Loading

--- src/next.config.js
+++ src/next.config.js
@@ -8,47 +8,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    allowedDevOrigins: [
-      "https://*.cluster-f4iwdviaqvc2ct6pgytzw4xqy4.cloudworkstations.dev",
-    ],
-  },
-  webpack(config, { isServer }) {
-    config.experiments = {
-      ...config.experiments,
-      topLevelAwait: true,
-    };
-    
-    // Mark server-side modules as external to prevent bundling errors
-    if (!isServer) {
-        config.externals = {
-            ...config.externals,
-            'express': 'express',
-            'handlebars': 'handlebars',
-            'require-in-the-middle': 'require-in-the-middle',
-            '@opentelemetry/api': '@opentelemetry/api',
-            '@opentelemetry/sd-node': '@opentelemetry/sd-node',
-            '@opentelemetry/resources': '@opentelemetry/resources',
-            '@opentelemetry/semantic-conventions': '@opentelemetry/semantic-conventions',
-            '@opentelemetry/instrumentation': '@opentelemetry/instrumentation',
-            '@opentelemetry/exporter-jaeger': '@opentelemetry/exporter-jaeger',
-        };
-    }
-    
-    config.module.rules.push({
-      test: /\.js$/,
-      include: /node_modules\/handlebars/,
-      use: {
-        loader: 'string-replace-loader',
-        options: {
-          search: 'require.extensions',
-          replace: '[]',
-        },
-      },
-    });
-
-    return config;
-  },
   images: {
     remotePatterns: [
       {

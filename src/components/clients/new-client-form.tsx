2abaed340786fd14536a80e5f8a775ddba1eb87b
JOSE ALEJANDRO PACHECO BARCENAS <vatosalfa@gmail.com>
2025-12-10 00:13:22 +0000

    sigue el mismo fallo

--- src/components/clients/new-client-form.tsx
+++ src/components/clients/new-client-form.tsx
@@ -385,7 +385,6 @@ export function NewClientForm({ onFormSubmit, client = null }: NewClientFormProp
                   <PopoverContent 
                     className="w-96 p-4 space-y-4" 
                     align="start"
-                    onOpenAutoFocus={(e) => e.preventDefault()}
                   >
                     <p className="font-semibold text-center">{field.value ? format(field.value, 'PPP', { locale: es }) : 'Selecciona una fecha'}</p>
                     <div className="grid grid-cols-3 gap-2">
--- src/components/reservations/new-reservation-form.tsx
+++ src/components/reservations/new-reservation-form.tsx
@@ -584,7 +584,9 @@ export function NewReservationForm({ isOpen, onOpenChange, onFormSubmit, initial
                               </Button>
                             </FormControl>
                           </PopoverTrigger>
-                          <PopoverContent className="w-auto p-0"><Calendar locale={es} mode="single" selected={field.value} onSelect={field.onChange} disabled={(date) => date < new Date(new Date().setHours(0,0,0,0))} initialFocus /></PopoverContent>
+                          <PopoverContent className="w-auto p-0" align="start" onOpenAutoFocus={(e) => e.preventDefault()}>
+                              <Calendar locale={es} mode="single" selected={field.value} onSelect={field.onChange} disabled={(date) => date < new Date(new Date().setHours(0,0,0,0))} initialFocus />
+                          </PopoverContent>
                         </Popover>
                         <FormMessage />
                       </FormItem>
